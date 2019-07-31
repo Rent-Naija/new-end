fbf996a7989e5d3cd47c9166aef4167fcda0b4cd
Ade Niyi <adesanoye.samson@yahoo.com>
2019-07-31 12:44:34 -0700

    define an interface for properties

---
+++ src/app/properties/property.ts
@@ -0,0 +1,13 @@
+export interface Iproperty {
+  id: number,
+  ownerid: string,
+  sagent: string,
+  name: string,
+  price: string,
+  type: string,
+  state: string,
+  city: string,
+  coverphoto: File,
+  address: string,
+  description: string
+}
