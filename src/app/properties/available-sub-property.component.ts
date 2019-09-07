76bb098ea467b4f15ff7ffabc66088188cd1a39f
Ade Niyi <adesanoye.samson@yahoo.com>
2019-09-07 14:46:31 -0700

    debug error and removed unwanted tags

--- src/app/properties/available-sub-property.component.ts
+++ src/app/properties/available-sub-property.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-available-sub-property',
   templateUrl: './available-sub-property.component.html',
-  styleUrls: ['./property.component.css']
+  styleUrls: ['./property.css']
 })
 export class AvailableSubPropertyComponent implements OnInit {
 @Input() availablesubp: any;
