65a64c023838ba9bf6d9177218a91570682d12d3
Ade Niyi <adesanoye.samson@yahoo.com>
2019-09-07 15:14:14 -0700

    move the dashboard menu to component

---
+++ src/app/admin/left-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-left-menu',
+  templateUrl: './left-menu.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class LeftMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
